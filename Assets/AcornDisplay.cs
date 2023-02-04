7eaa5f179282c93ed7740945d580c0f1bee0cd30
Guest Person <gst15202@hs.local>
2023-02-04 14:48:14 +0100

    add acorn display even though it doesnt work yet my computer is messed up and i need to restart it so idk what else to do

---
+++ Assets/AcornDisplay.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcornDisplay : MonoBehaviour
+{
+    public void UpdateDisplay()
+    {
+               
+    }
+}
