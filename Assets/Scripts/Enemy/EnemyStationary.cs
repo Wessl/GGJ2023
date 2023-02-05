59cbb13028ae45ab04a51f442498c38a56b27429
GodEmperorDaniel <40898892+GodEmperorDaniel@users.noreply.github.com>
2023-02-05 11:41:49 +0100

    Static enemy anim

--- Assets/Scripts/Enemy/EnemyStationary.cs
+++ Assets/Scripts/Enemy/EnemyStationary.cs
@@ -2,25 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStationary : MonoBehaviour
+public class EnemyStationary : EnemyBase
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            Debug.Log("Oh no, I am die, thank you forever");
-        }
-    }
 }
