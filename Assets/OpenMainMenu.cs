b2ee1d4594e1a82c41f9df9f484048513bf065a5
arget4me <simon.johansson@piktiv.se>
2023-02-05 14:38:18 +0100

    Fix winscene

---
+++ Assets/OpenMainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenMainMenu : MonoBehaviour
+{
+
+    float elapsed = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsed = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (elapsed + 20.0f <= Time.time)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+
+
+
+
+}
